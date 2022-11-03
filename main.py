b7cf6f83d0f663901129dce650c002fc0cae55d9
AG <gastandrzej@gmail.com>
2022-11-03 11:57:35 +0100

    Lekcja 1

---
+++ main.py
@@ -0,0 +1,73 @@
+"""
+TYPY DANYCH
+"""
+
+# Liczby
+# int(9)
+# float(8.9)
+# print(math.pi)
+#
+
+# Słowa
+# str("slowo")
+# rodzaje stringów
+# c = r"C:\game\node"
+# print(f"bardzo fajny string {54 + math.pi}")
+# \n - następna linijka
+# char = "@"
+
+# Listy
+# lista_g = ["Hello", "World", " ", "!", 54]
+
+# Dane wprowadzone przez użytkownika
+# x = input("Wprowadź liczbę ")
+
+#
+# def func1():
+#     # return print(f"Jeżeli do {x} dodasz 41 to wynikiem takiego działania będzie {x+41}")
+#     return print()
+# #
+# func1()
+
+
+'''
+
+Napisać funkcje dodawania, odejmowania, mnożenia, dzielenia i procent z danej liczby
+
+Wszystkie z nich mają działać poprzez dane wprowadzone przez użytkownika :)
+
+Odpowiedź ma być zwracana pełnym zdaniem :)
+'''
+# '''
+#
+# import pandas
+#
+#
+# def func1():
+#     pass
+#
+#
+#
+#
+#
+#
+# if __name__ == "__main__":
+#     func1()
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
