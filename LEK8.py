0e2318fd7b5ca698e1b03188f538dc9184ff5a4a
AG <gastandrzej@gmail.com>
2022-11-25 15:23:50 +0100

    lekcja 8

---
+++ LEK8.py
@@ -0,0 +1,151 @@
+T1 = ['v', 'N', 'k', 402, 220, 'x', 66, 'A', 445, 453, 66, 402, 'i', 'p', 170, 'D', 't', 633, 'C', 'Q', 303, 'j', 821, 'r', 220, 332, 'M', 'i', 'e', 'J', 'y', 'r', 'V', 'p', 695, 402, 257, 'O', 'G', 'x', 'j', 'F', 'w', 695, 155, 'Y', 'x', 'c', 524, 'W', 'B', 'y', 'm', 'N', 223, 257, 'v', 445, 'N', 'F', 'F', 'V', 702, 287, 'Z', 445, 'u', 155, 524, 'R', 11, 445, 170, 303, 170, 287, 695, 'j', 821, 96, 11, 590, 444, 985, 'm', 'Z', 453, 470, 'j', 'u', 'w', 66, 'g', 'b', 590, 220, 'z', 'T', 287, 223, 590, 'A', 11, 'w', 'b', 'B', 985, 885, 155, 'F', 'S', 'U', 'k', 524, 194, 11, 220, 't', 's', 444]
+T2 = ['p', 'S', 's', 26, 'P', 'A', 'P', 'l', 358, 405, 19, 'n', 924, 554, 'T', 378, 8, 'x', 903, 'g', 130, 'G', 26, 554, 'b', 'F', 'h', 'I', 'w', 358, 'A', 'R', 214, 's', 98, 468, 26, 884, 'S', 'a', 903, 'H', 't', 'X', 554, 29, 8, 'f', 'f', 'u', 'h', 214, 'a', 686, 358, 'p', 'G', 'J', 'F', 884, 'x', 884, 19, 'P', 'W', 934, 'q', 903, 'e', 'K', 's', 's', 'h', 115, 181, 'u', 995, 631, 990, 37, 308, 137, 'Q', 'R', 'P', 340, 'z', 118, 'Y', 'r', 'f', 358, 26, 340, 934, 221, 934, 'o', 951, 137, 995, 19, 521, 'f', 'x', 137, 'O', 468, 'w', 'm', 'Q', 'V', 'N', 'p', 'Z', 686, 995, 137, 995, 137]
+T3 = [485, 183, 786, 786, 'X', 309, 340, 'o', 147, 'y', 'x', 'e', 'd', 686, 725, 358, 'V', 106, 657, 'Y', 539, 'V', 654, 803, 'o', 'v', 's', 'r', 883, 'p', 781, 'N', 'z', 'S', 930, 883, 'B', 'I', 469, 183, 309, 406, 'j', 654, 'E', 406, 'g', 'f', 371, 106, 'R', 'u', 340, 'e', 'g', 'v', 'D', 'I', 'S', 'O', 433, 469, 'v', 'h', 'r', 'j', 803, 106, 'B', 'b', 'W', 930, 'p', 'M', 'k', 406, 'o', 137, 485, 'i', 'W', 'W', 'U', 423, 'x', 371, 'l', 'V', 'U', 'T', 'B', 343, 657, 'N', 'J', 781, 'O', 'X', 469, 725, 'U', 'j', 'w', 803, 340, 'p', 414, 417, 'Z', 'M', 469, 'x', 'z', 'M', 'w', 686, 371, 417, 343, 'z']
+T4 = ['Y', 's', 'O', 's', 858, 'r', 964, 683, 's', 'e', 597, 1, 273, 532, 921, 'H', 'z', 'x', 472, 43, 'L', 246, 202, 'k', 472, 'Q', 'j', 532, 921, 't', 'S', 'p', 1, 'j', 696, 689, 'U', 'W', 964, 683, 't', 'L', 964, 'D', 125, 'R', 'L', 389, 's', 'e', 'Z', 259, 1, 'M', 259, 459, 'V', 'E', 'n', 'c', 'F', 'v', 696, 'l', 683, 'o', 597, 459, 'm', 696, 'D', 597, 617, 427, 'i', 125, 718, 'u', 43, 'f', 'Q', 'J', 'b', 'l', 43, 760, 'l', 'F', 'h', 'r', 'R', 88, 'C', 'r', 'A', 'X', 591, 'r', 273, 'g', 858, 'J', 459, 'c', 'L', 273, 'p', 'e', 43, 597, 'F', 'c', 389, 'H', 'L', 's', 661, 'P', 'K', 202]
+T5 = ['b', 'd', 'E', 414, 415, 232, 't', 584, 255, 'O', 'f', 'z', 'q', 187, 'r', 'W', 944, 414, 'v', 413, 'k', 255, 584, 696, 'I', 'z', 'C', 889, 'a', 'Y', 'E', 'y', 971, 949, 417, 971, 701, 'u', 'L', 'W', 577, 111, 'y', 889, 415, 'g', 'E', 'b', 'h', 971, 'U', 258, 971, 187, 949, 'I', 'X', 848, 'C', 'Z', 640, 'f', 392, 889, 348, 'Z', 'i', 'e', 255, 'g', 'Q', 'G', 'o', 'L', 'h', 749, 170, 111, 'H', 736, 686, 606, 971, 232, 'm', 'D', 170, 'N', 971, 'L', 'r', 'f', 348, 74, 584, 'K', 'j', 944, 'K', 686, 584, 'H', 'y', 606, 'I', 'z', 'D', 187, 'D', 'X', 187, 170, 255, 944, 'G', 749, 392, 971, 'y', 'D']
+T6 = ['p', 47, 'S', 256, 4, 'O', 'N', 768, 946, 652, 413, 47, 'C', 'o', 133, 't', 'g', 91, 'r', 'S', 'e', 768, 909, 256, 171, 'M', 'S', 909, 272, 'K', 931, 't', 256, 752, 'L', 605, 'f', 931, 'r', 'e', 'K', 'E', 605, 168, 'l', 4, 'd', 'i', 482, 'l', 'A', 'a', 'G', 'H', 'h', 931, 91, 931, 'g', 'D', 'X', 144, 715, 256, 652, 'q', 144, 'A', 'y', 70, 'D', 'M', 'g', 'F', 499, 'y', 413, 482, 752, 'k', 768, 'P', 'v', 'Y', 't', 'e', 652, 'K', 133, 's', 195, 652, 'v', 47, 256, 499, 171, 'q', 'z', 91, 'w', 47, 641, 'J', 'q', 'q', 195, 768, 'K', 605, 283, 'M', 'L', 133, 'm', 'y', 'D', 'o', 133, 218]
+T7 = [168, 559, 408, 931, 338, 931, 392, 'O', 474, 41, 553, 671, 'C', 'q', 's', 'a', 805, 625, 553, 478, 'V', 76, 628, 392, 559, 341, 'R', 478, 'Y', 628, 'z', 341, 805, 41, 805, 392, 313, 559, 'j', 616, 313, 341, 504, 917, 338, 168, 358, 'u', 474, 'X', 442, 168, 'L', 'C', 671, 338, 'I', 917, 'Q', 'n', 'I', 'V', 341, 'Z', 628, 't', 'h', 't', 628, 44, 'D', 'u', 358, 866, 866, 'K', 504, 168, 559, 495, 733, 'k', 'v', 610, 'T', 'V', 504, 341, 478, 'z', 'B', 'a', 'Z', 'f', 'e', 495, 'E', 553, 338, 41, 168, 'k', 341, 293, 'i', 'c', 'w', 'O', 504, 'G', 866, 'E', 44, 'd', 'V', 628, 313, 'O', 'x', 392]
+T8 = ['a', 'y', 'Z', 'c', 'i', 'a', 'A', 152, 71, 't', 'j', 'L', 'a', 796, 'i', 890, 'I', 'C', 281, 596, 7, 250, 'i', 783, 'D', 92, 'z', 'a', 71, 'f', 'q', 'C', 'k', 458, 526, 458, 281, 560, 'F', 458, 336, 336, 225, 796, 's', 'o', 'Q', 783, 768, 890, 's', 767, 'C', 768, 767, 713, 783, 'T', 767, 'T', 35, 767, 'v', 'k', 'C', 'l', 'O', 'C', 669, 225, 'z', 'W', 336, 796, 'c', 768, 'J', 'c', 'P', 636, 'A', 'g', 'a', 596, 'm', 'u', 35, 'X', 'k', 768, 'p', 123, 'E', 'j', 636, 'h', 250, 'C', 'k', 'J', 'M', 'o', 'w', 'C', 'D', 596, 669, 981, 92, 'N', 560, 225, 281, 669, 'u', 'y', 123, 949, 768, 281]
+T9 = [808, 780, 'F', 'F', 'x', 115, 'O', 586, 947, 'K', 35, 168, 808, 'B', 'p', 810, 407, 'U', 308, 8, 808, 683, 'V', 'e', 780, 586, 'Z', 'Q', 34, 683, 's', 780, 'z', 338, 'y', 549, 'I', 'B', 'S', 'h', 278, 308, 109, 'j', 'n', 'r', 56, 'r', 'x', 278, 180, 407, 947, 'P', 549, 808, 407, 'R', 'j', 'W', 762, 808, 'U', 'i', 'I', 338, 921, 'K', 'c', 'q', 762, 'J', 586, 109, 549, 692, 921, 586, 452, 'V', 's', 762, 180, 'O', 'C', 's', 683, 'A', 692, 244, 'I', 'd', 452, 34, 'k', 921, 34, 407, 'Q', 586, 792, 338, 'G', 'J', 56, 'Y', 'J', 407, 'c', 407, 115, 780, 'G', 'V', 780, 159, 'v', 'T', 921, 'h']
+T10 = [341, 691, 'o', 970, 577, 'S', 'S', 'F', 'f', 603, 'k', 'd', 'M', 'h', 'W', 'c', 'G', 371, 'D', 'q', 's', 142, 'g', 341, 'f', 'r', 's', 875, 364, 107, 'x', 108, 'H', 'Z', 313, 'T', 970, 816, 'e', 'J', 'R', 'I', 364, 299, 341, 299, 'X', 728, 364, 'x', 107, 151, 875, 382, 341, 108, 'F', 'v', 333, 875, 'W', 577, 374, 364, 526, 'k', 370, 's', 'K', 728, 299, 603, 108, 'E', 728, 970, 'Z', 'D', 'Y', 313, 'b', 'u', 603, 'Z', 151, 299, 'J', 875, 'U', 'C', 'M', 107, 383, 'X', 'Y', 382, 'W', 266, 'a', 'k', 371, 333, 526, 577, 728, 'D', 738, 'j', 299, 'm', 'i', 816, 142, 'C', 371, 831, 151, 370, 'n', 577]
+
+
+'''
+Czy elementy w liście T1 są parzyste czy nieparzyste
+
+napisz powiadomienie dla każdej wartości wewnątrz niej używając fstring
+
+wzór:
+element wartość_elementu jest _parzysty_bądź_nieparzysty_
+
+'''
+
+
+
+
+
+
+'''
+Projektowania:
+
+funkcja: przyjmuje wartości listy:
+
+
+        ROZRÓŻNIENIE TYPU WARTOŚCI:
+        
+    
+    najpierw musimy przez liste przejść?
+        pętla for:
+            wartości wew listy
+            
+            
+            wywalamy wartości do int jakie narzędzie zostanie użyte? 
+                try: -sprawdzenie wartości, podniesienie do int
+                        
+                        ZAGADNIENIE LICZB PARZYSTYCH
+                             ((Liczba)mod2 == 0) ---> wyższy stopień skomplikowania warunku
+                     
+                     instrukcja warunkowa czy parzysta if:
+                                    True: parzysta
+                                        wbudow print(f"liczba {liczba} jest parzysta)
+                                        
+                                    False: nparz
+                                        wbudow print(f"liczba {liczba} jest parzysta)
+                
+                :except
+                    pass
+         
+'''
+
+
+
+
+# def func1(x):
+#     for i in x:
+#         try:
+#             int(i)
+#             if i%2 == 0:
+#                 print(f"Liczba {i} jest parzysta")
+#             else:
+#                 print(f"Liczba {i} jest parzysta")
+#         except:
+#             pass
+#
+# func1(T1)
+
+'''
+
+Zadanie
+    suma elementów w liście jest kluczem, sprawdź która liczba jest potrzebna do jej odszyfrowania z pomocą modulacji
+    
+    
+
+
+'''
+
+'''
+lista wchodzi do funkcji
+
+najmniejszy możliwy dzielnik sumy elementów w liście który podzieli ją całkowicie
+
+
+większy od 5
+
+
+
+czy istnieje jakaś funkcja lub biblioteka która podstawi nam elementy do podziału?
+'''
+
+'''
+projekt
+
+func(x)
+    zmienna_div = c = 5
+    zmienna_zero
+    for tylko przejście (i to elementy wew listy)
+        określenie wartości ---> try do int()
+            zmienna_zer0 = zmienna_zer0 + i
+        :except
+            pass
+
+    for i in range(5, len(b))
+        if i % c == 0:
+            print(f" kluczem do listy jest {c})
+        else:
+            c = c + 1 
+
+'''
+#
+# def func1(x):
+#     c = 5
+#     b = 0
+#     for i in x:
+#         try:
+#             int(i)
+#             b = b+1
+#         except:
+#             pass
+#     for i in range(5,b):
+#         if b%c == 0:
+#             print(f"Klucz to {c}")
+#             break
+#         else:
+#             c = c+1
+#
+# func1(T1)
+
+'''
+zad domowe
+
+zaprojektuj i napisz funkcje która dodaje do listy kolejne elementy z zewnątrz (wpisane przez użytkownika)
+zaprojektuj i napisz funkcje która dodaje do listy kolejne elementy z zewnątrz (wpisane przez użytkownika ale tylko liczby)
+zaprojektuj i napisz funkcje która wyrzuci nam ostatni integer z listy
+zaprojektuj i napisz funkcje która wyrzuci nam ostatni element
+zaprojektuj i napisz funkcje która wyrzuci nam ostatni string z listy
+zaprojektuj i napisz funkcje która dodaje losowe elementy do listy
+zaprojektuj i napisz funkcje która sumuje elementy liczbowe w liście i podaje ich najmniejszy wspólny nieparzysty dzielnik
+wyższy od 18
+
+zaprojektuj i napisz funkcje która sumuje elementy liczbowe w liście i podaje ich najmniejszy wspólny parzysty dzielnik
+wyższy od 34
+'''
